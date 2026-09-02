1d5e40713b7f4863827fd466ffc709f767dbb5ae
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-09-02 19:07:20 +0530

    updated

---
+++ math.py
@@ -0,0 +1,8 @@
+import math
+
+def get_factorial_built_in(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
+    return math.factorial(n)
+
+print(get_factorial_built_in(5)) 
